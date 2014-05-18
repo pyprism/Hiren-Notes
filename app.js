4a75163aa06d23bcc2889d1880b14839b8cd1fb4
pyprism <git.pyprism@gmail.com>
2014-05-18 20:21:18 +0600

    Added sexy Handlebar.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.engine('hbs', cons.handlebars);
+app.set('view engine', 'hbs');
 
 app.use(favicon());
 app.use(logger('dev'));
