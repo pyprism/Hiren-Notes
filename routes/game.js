8ad11b864fa748a19343f700bfc177790b3c9cab
pyprism <git.pyprism@gmail.com>
2014-05-21 11:45:29 +0600

    new route for game

--- routes/game.js
+++ routes/game.js
@@ -1,3 +1,11 @@
 /**
  * Created by prism on 5/21/14.
  */
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('tic-tac-toe');
+});
+
+module.exports = router;
