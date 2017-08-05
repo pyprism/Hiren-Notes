31f20550a60ef44387f1d71fd74a6fc8890ddb65
Ashutosh Das <git.pyprism@gmail.com>
2017-08-05 20:47:45 +0600

    Added new url

--- hiren/urls.py
+++ hiren/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from note import views
+
+router = routers.DefaultRouter()
+router.register(r'notes', views.NotesViewset)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
 ]
