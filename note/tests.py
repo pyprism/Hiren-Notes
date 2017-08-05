c96c44744376d9bfd59ccf708a518b347df477ee
Ashutosh Das <git.pyprism@gmail.com>
2017-08-05 21:09:23 +0600

    wip

--- note/tests.py
+++ note/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, TransactionTestCase
+from .models import Notes
+from django.utils import timezone
+from rest_framework.test import APIRequestFactory, APIClient
+from django.contrib.auth.models import User
+from freezegun import freeze_time
+import json
