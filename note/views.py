7df8a8ae1f47a560c9e70487fafe091b890bf48a
Ashutosh Das <git.pyprism@gmail.com>
2017-08-05 20:48:24 +0600

    added view

--- note/views.py
+++ note/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import NotesSerializer
+from .models import Notes
 
-# Create your views here.
+
+class NotesViewset(viewsets.ModelViewSet):
+    """
+        API endpoint that allows notes to be created, viewed ,edited and deleted.
+    """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Notes.objects.all()
+    serializer_class = NotesSerializer
+
