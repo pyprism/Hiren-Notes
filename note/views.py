044b4e328e42b3137832dd825a66782670e58c53
Ashutosh Das <git.pyprism@gmail.com>
2017-08-05 20:50:29 +0600

    added token view

--- note/views.py
+++ note/views.py
@@ -4,6 +4,26 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import NotesSerializer
 from .models import Notes
+from rest_framework.authtoken.models import Token
+from django.contrib import auth
+from django.http import JsonResponse
+
+
+def login(request):
+    """
+    token authentication for api
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            token = Token.objects.get_or_create(user=user)
+            return JsonResponse({'token': str(token[0])})
+        else:
+            return JsonResponse({'error': 'Username/Password is not valid'})
 
 
 class NotesViewset(viewsets.ModelViewSet):
