b0582b65dde3497a83168a0206507c1ac7ae2a88
Ashutosh Das <git.pyprism@gmail.com>
2017-08-05 20:47:58 +0600

    added notes model

--- note/models.py
+++ note/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    content = models.TextField()
+    iv = models.CharField(max_length=500)
+    salt = models.CharField(max_length=1000)
+    synced_desktop = models.BooleanField(default=False)
+    synced_mobile = models.BooleanField(default=False)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
