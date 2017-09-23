0421e1d34d1ce3afdae6b59367d988f9a7d82759
Ashutosh Das <git.pyprism@gmail.com>
2017-09-23 11:01:29 +0600

    Added reactjs view

--- note/views.py
+++ note/views.py
@@ -9,6 +9,11 @@ from django.contrib import auth
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
+from django.views.generic import View
+from hiren import settings
+from django.http import HttpResponse
+import os
+import logging
 
 
 def login(request):
@@ -26,6 +31,28 @@ def login(request):
             return JsonResponse({'token': str(token[0])}, status=status.HTTP_200_OK)
         else:
             return JsonResponse({'error': 'Username/Password is not valid'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class BunnyAppView(View):
+    """
+    Serves the compiled frontend entry point (only works if you have run `npm
+    run build`).
+    """
+
+    def get(self, request):
+        try:
+            with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            logging.exception('Production build of app not found')
+            return HttpResponse(
+                """
+                This URL is only used when you have built the production
+                version of the app. Visit http://localhost:3000/ instead, or
+                run `npm run build` to test the production version.
+                """,
+                status=501,
+            )
 
 
 class NotesViewset(viewsets.ModelViewSet):
