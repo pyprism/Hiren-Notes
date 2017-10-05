a9fe5be537a746ba557a9ccd6680ec2ed223c2e9
Ashutosh Das <git.pyprism@gmail.com>
2017-10-05 11:39:32 +0600

    wip

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Login from "components/Login";
+// import "static/css/bootstrap.min.css";
+// import "static/css/waves.min.css";
+// import "static/css/animate.min.css";
+// import "static/css/style.min.css";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Route exact path="/" component={Login} />
+            </Router>
+        );
+    }
 }
 
 export default App;
