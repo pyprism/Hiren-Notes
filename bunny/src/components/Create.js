0e8f0632325075c10ba1d5a53ade0468d55aa6d5
Ashutosh Das <git.pyprism@gmail.com>
2017-10-11 12:57:44 +0600

    wip

--- bunny/src/components/Create.js
+++ bunny/src/components/Create.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 import {Helmet} from "react-helmet";
 import axios from "axios";
+import Sidebar from "Sidebar";
 
 export default class Create extends React.Component {
     render() {
         return (
             <div>
+                <Sidebar/>
                 <Helmet>
                     <title>Hiren-Notes: Create New Note</title>
                 </Helmet>
-                create
+                <section className="content">
+                    <div className="container-fluid">
+                        create
+                    </div>
+                </section>
+
             </div>
         );
     }
