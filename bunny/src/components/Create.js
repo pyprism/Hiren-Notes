45423e0042ce624755a5751d38f61de82af6dc3a
Ashutosh Das <git.pyprism@gmail.com>
2017-10-11 12:49:27 +0600

    added form component

---
+++ bunny/src/components/Create.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Helmet} from "react-helmet";
+import axios from "axios";
+
+export default class Create extends React.Component {
+    render() {
+        return (
+            <div>
+                <Helmet>
+                    <title>Hiren-Notes: Create New Note</title>
+                </Helmet>
+                create
+            </div>
+        );
+    }
+}
