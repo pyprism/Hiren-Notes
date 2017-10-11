6b9e569410bb07226dded0de7f84c8fc4b9814cc
Ashutosh Das <git.pyprism@gmail.com>
2017-10-11 12:52:21 +0600

    removed unused line

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Login from "components/Login";
-// import "static/css/bootstrap.min.css";
-// import "static/css/waves.min.css";
-// import "static/css/animate.min.css";
-// import "static/css/style.min.css";
 
 
 class App extends Component {
