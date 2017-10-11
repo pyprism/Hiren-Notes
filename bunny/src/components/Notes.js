d37cb113275bd81a049cc902071d43d31e4f7b36
Ashutosh Das <git.pyprism@gmail.com>
2017-10-11 12:57:59 +0600

    wip

--- bunny/src/components/Notes.js
+++ bunny/src/components/Notes.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Helmet} from "react-helmet";
+import Sidebar from "Sidebar";
 
 export default class Notes extends React.Component {
     render() {
         return (
             <div>
+                <Sidebar/>
                 <Helmet>
                     <title>Hiren-Notes: All Notes</title>
                 </Helmet>
-                Notes
+                <section className="content">
+                    <div className="container-fluid">
+                        Notes
+                    </div>
+                </section>
             </div>
         );
     }
