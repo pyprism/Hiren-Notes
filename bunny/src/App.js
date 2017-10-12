bb61e1da756e1fe28d965f7d198e3d52878f4b8b
Ashutosh Das <git.pyprism@gmail.com>
2017-10-12 17:27:01 +0600

    added new route

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Login from "components/Login";
 import Notes from "components/Notes";
 import Create from "components/Create";
 
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <Router>
-                <Route exact path="/" component={Login} />
-                <Route exact path="/notes" component={Notes} />
-                <Route exact path="/create" component={Create} />
+                <div>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/notes" component={Notes} />
+                    <Route path="/create" component={Create} />
+                </div>
             </Router>
         );
     }
