0e910029a358e57ee754f920332ddbc93bca4f69
Ashutosh Das <git.pyprism@gmail.com>
2017-12-03 14:32:08 +0600

    fixed errorS

--- note/serializers.py
+++ note/serializers.py
@@ -10,6 +10,11 @@ class NoteBookSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoteBook
         fields = '__all__'
+        extra_kwargs = {
+            'name': {
+                'validators': [],  # disable unique validator
+            }
+        }
 
 
 class NotesSerializer(serializers.ModelSerializer):
@@ -19,20 +24,21 @@ class NotesSerializer(serializers.ModelSerializer):
     unique_id = serializers.ReadOnlyField()
     created_at = serializers.ReadOnlyField()
     updated_at = serializers.ReadOnlyField()
-    notebook = NoteBookSerializer(many=True)
+    note_book = NoteBookSerializer()
 
     class Meta:
         model = Notes
-        fields = ('id', 'notebook', 'title', 'content', 'encrypted', 'iv',
+        fields = ('id', 'note_book', 'title', 'content', 'encrypted', 'iv',
                   'salt', 'synced_desktop', 'synced_mobile', 'unique_id', 'created_at', 'updated_at', 'user')
 
     def create(self, validated_data):
-        notebook_data = validated_data.pop('notebook')
-        print(notebook_data)
+        notebook_data = validated_data.pop('note_book')
         notebook = NoteBook.objects.filter(name=notebook_data['name'])
         if notebook.exists():
+            print('hit')
             hiren = Notes.objects.create(note_book=notebook[0], **validated_data)
         else:
+            print('hot')
             notebook_obj = NoteBook.objects.create(**notebook_data)
             hiren = Notes.objects.create(note_book=notebook_obj, **validated_data)
         return hiren
