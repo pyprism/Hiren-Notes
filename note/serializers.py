17ecdbd66f32488a65c7223cab40ea9bc44c1a4b
Ashutosh Das <git.pyprism@gmail.com>
2017-12-03 11:54:20 +0600

    wip

--- note/serializers.py
+++ note/serializers.py
@@ -16,10 +16,26 @@ class NotesSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
-    note_book = NoteBookSerializer(many=True)
+    unique_id = serializers.ReadOnlyField()
+    created_at = serializers.ReadOnlyField()
+    updated_at = serializers.ReadOnlyField()
+    notebook = NoteBookSerializer(many=True)
 
     class Meta:
         model = Notes
-        fields = '__all__'
+        fields = ('id', 'notebook', 'title', 'content', 'encrypted', 'iv',
+                  'salt', 'synced_desktop', 'synced_mobile', 'unique_id', 'created_at', 'updated_at', 'user')
+
+    def create(self, validated_data):
+        notebook_data = validated_data.pop('notebook')
+        print(notebook_data)
+        notebook = NoteBook.objects.filter(name=notebook_data['name'])
+        if notebook.exists():
+            hiren = Notes.objects.create(note_book=notebook[0], **validated_data)
+        else:
+            notebook_obj = NoteBook.objects.create(**notebook_data)
+            hiren = Notes.objects.create(note_book=notebook_obj, **validated_data)
+        return hiren
+
 
 
