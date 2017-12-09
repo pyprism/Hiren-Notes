d61308c4f8ad8e02062d59b6e3824738dd4b3380
Ashutosh Das <git.pyprism@gmail.com>
2017-12-09 11:55:13 +0600

    added new route

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -4,15 +4,18 @@ import Login from "components/Login";
 import Notes from "components/Notes";
 import Create from "components/Create";
 import NoteBook from "components/NoteBook";
+import {Notebook as NotebookStore } from "./stores/Notebook";
+
 
 
 class App extends React.Component {
+    noteStore = new NotebookStore();
     render() {
         return (
             <Router>
                 <div>
                     <Route exact path="/" component={Login} />
-                    <Route path="/notebook" component={NoteBook} />
+                    <Route path="/notebook" render={(props) => (<NoteBook notebook={this.noteStore} {...props}/>)} />/>
                     <Route path="/notes" component={Notes} />
                     <Route path="/create" component={Create} />
                 </div>
