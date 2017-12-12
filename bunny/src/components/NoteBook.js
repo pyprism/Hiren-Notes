9aad49e6cebc91fae55a7b01b725b427ef5e94d6
Ashutosh Das <git.pyprism@gmail.com>
2017-12-12 11:32:50 +0600

    Added more useless lines

--- bunny/src/components/NoteBook.js
+++ bunny/src/components/NoteBook.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import {Helmet} from "react-helmet";
 import Sidebar from "Sidebar";
+import { observer } from "mobx-react";
+import { toJS } from "mobx";
 
-export default class NoteBook extends React.Component {
+
+let NoteBook = observer(class NoteBook extends React.Component {
 
     componentDidMount(){
         this.props.notebook.getNotebook();
+    }
+
+    bunny() {
+        console.log(toJS(this.props.notebook.notebook));
+        console.log(toJS(this.props.notebook.loadingText));
+        return (this.props.notebook.notebook).map((data, index) => {
+            return (
+                <div className="col-lg-4 col-md-4 col-sm-6 col-xs-12" key={ data.id }>
+                    <div className="card">
+                        <div className="header">
+                            <h2>
+                                { data.name }
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
     }
 
     render() {
@@ -20,10 +41,12 @@ export default class NoteBook extends React.Component {
                         Note book
                     </div>
                     <div className="row clearfix">
-
+                        {this.bunny()}
                     </div>
                 </section>
             </div>
         );
     }
-}
+});
+
+export default NoteBook;
