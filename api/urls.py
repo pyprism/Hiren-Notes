9c49dc78def1219a69948d26bb79d6d3e950ba15
Ashutosh Das <git.pyprism@gmail.com>
2018-02-26 18:17:39 +0600

    added new url

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'notebook', views.NoteBookViewset)
+router.register(r'notes', views.NotesViewset)
+
+urlpatterns = [
+    path('/auth/', views.login),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('/', include(router.urls)),
+    path('', views.BunnyAppView.as_view()),
+]
+
+
