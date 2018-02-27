2e7090d75d4d59b6fb8d4687ecb507f68f467eca
Ashutosh Das <git.pyprism@gmail.com>
2018-02-27 09:44:37 +0600

    fixed build

--- api/tests.py
+++ api/tests.py
@@ -3,6 +3,7 @@ from note.models import Notes, NoteBook
 from django.utils import timezone
 from rest_framework.test import APIRequestFactory, APIClient
 from hiren.settings import AUTH_USER_MODEL as User
+from base.models import Account
 from freezegun import freeze_time
 import uuid
 from unittest.mock import patch
@@ -18,7 +19,7 @@ class NotesViewTest(TransactionTestCase):
     # @patch.object(uuid, 'uuid4', side_effect=TEST_UUID)
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user('hiren', 'password')
+        self.user = Account.objects.create_user('hiren', 'password')
         self.client.force_authenticate(user=self.user)
         self.TEST_UUID = '2e49f1f0-e17b-4876-9beb-4d9b539683d8'
         with patch.object(uuid, 'uuid4', side_effect=self.TEST_UUID):
@@ -42,7 +43,7 @@ class LoginViewTest(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user('hiren', 'password')
+        self.user = Account.objects.create_user('hiren', 'password')
 
     def test_login_failed(self):
         response = self.client.post('/api/auth/', {'username': 'hire', 'password': 'password'})
