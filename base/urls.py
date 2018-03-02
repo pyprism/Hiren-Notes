656a626cbaffca80b9d4bde3752f3b9df8f1308c
Ashutosh Das <git.pyprism@gmail.com>
2018-03-02 10:05:30 +0600

    added new route

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
 
