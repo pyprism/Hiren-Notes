d799d89e09a9fc3bcb9846622f5f013a3e1f8ffe
Ashutosh Das <git.pyprism@gmail.com>
2018-03-02 11:36:18 +0600

    wip

---
+++ note/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def notebook(request):
+    return render(request, 'note/notebook.html')
+
