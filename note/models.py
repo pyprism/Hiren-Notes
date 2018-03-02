eb01c9dcb794248d922663151fbe77b234088cc9
Ashutosh Das <git.pyprism@gmail.com>
2018-03-02 11:36:11 +0600

    refactored model

--- note/models.py
+++ note/models.py
@@ -1,25 +1,29 @@
 from django.db import models
-from hiren.settings import AUTH_USER_MODEL as User
+from base.models import Account as User
 import uuid
 
 
 class NoteBook(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=500, unique=True)
-    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.TextField()
+    encrypted = models.BooleanField(default=False)
+    iv = models.CharField(max_length=500, blank=True, null=True)
+    salt = models.CharField(max_length=1000, blank=True, null=True)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Notes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    note_book = models.ForeignKey(NoteBook, on_delete=models.PROTECT)
+    note_book = models.ForeignKey(NoteBook, on_delete=models.CASCADE)
     title = models.TextField()
+    title_salt = models.CharField(max_length=1000, blank=True, null=True)
     content = models.TextField()
     encrypted = models.BooleanField(default=False)
     iv = models.CharField(max_length=500, blank=True, null=True)
     salt = models.CharField(max_length=1000, blank=True, null=True)
-    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
