c859e01b68d29f4022e6a32fa026db983a89cc6c
Ashutosh Das <git.pyprism@gmail.com>
2018-03-02 10:05:06 +0600

    added settings model

--- base/models.py
+++ base/models.py
@@ -30,3 +30,15 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = 'username'
 
     objects = AccountManager()
+
+
+class Setting(models.Model):
+    active = models.BooleanField(default=True)
+    task_type = (
+        ('S', 'Signup'),
+    )
+    task = models.CharField(choices=task_type, max_length=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
