19a575069ffc6554d85fc69c2eee65d6732f21f2
Ashutosh Das <git.pyprism@gmail.com>
2018-03-02 11:35:29 +0600

    added new route

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
-from django.urls import include, path, re_path
+from django.urls import path
+from django.contrib.auth.views import logout
 from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_code, name='secret_code'),
+    path('logout/', logout, {'next_page': '/'}, name='logout'),
 ]
 
