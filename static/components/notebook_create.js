9273f9c3de2bbc943b35a5b058f4070c426f29fa
Ashutosh Das <git.pyprism@gmail.com>
2018-03-04 17:39:02 +0600

    added new template, component

---
+++ static/components/notebook_create.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+class NotebookCreate extends React.Component {
+    render() {
+        return(
+            <div>formuju</div>
+        )
+    }
+}
+
+ReactDOM.render(<NotebookCreate />, document.getElementById("notebook_create"));
