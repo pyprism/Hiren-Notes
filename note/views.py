a802885ea2e091e5f69c2dd3f845f7ef8f45fe65
Ashutosh Das <git.pyprism@gmail.com>
2018-03-05 17:54:45 +0600

    Added form saving

--- note/views.py
+++ note/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import NoteBook, Notes
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from .forms import NoteBookForm
 
 
 @login_required
@@ -17,6 +18,20 @@ def notebook(request):
 
 @login_required
 def notebook_create(request):
+    """
+    Handles notebook creation
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        notebook_form = NoteBookForm(request.POST)
+        if notebook_form.is_valid():
+            notebook = notebook_form.save(commit=False)
+            notebook.user = request.user
+            notebook.save()
+            return HttpResponse("success")
+        else:
+            return HttpResponse("error")
     if request.META.get('HTTP_ACCEPT').startswith("text/html"):
         return render(request, 'note/notebook_create.html')
     elif request.content_type == 'application/json':
