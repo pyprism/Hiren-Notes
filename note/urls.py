73f55ad9187918c47cf25e504f2eb07890793fa4
Ashutosh Das <git.pyprism@gmail.com>
2018-03-07 11:50:28 +0600

    added new route

--- note/urls.py
+++ note/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.notebook, name='notebook'),
+    path('', views.notebooks, name='notebook'),
+    path('<int:pk>/', views.notebook_by_id, name='notebook_by_id'),
     path('create/', views.notebook_create, name='notebook_create'),
 ]
 
