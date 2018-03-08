22ecfe68e852549d2d51516aafb844e607b01e56
Ashutosh Das <git.pyprism@gmail.com>
2018-03-08 10:57:30 +0600

    added new script

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require("path");
 module.exports = {
     entry: {
         "notebook": "./static/components/notebooks.js",
-        "notebook_create": "./static/components/notebook_create.js"
+        "notebook_create": "./static/components/notebook_create.js",
+        "notebook_by_id": "./static/components/notebook_by_id.js",
     },
     output: {
         path: path.resolve(__dirname, "static"),
