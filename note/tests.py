28d488515403c3a1051faaa9e3853fb3993c83dd
Ashutosh Das <git.pyprism@gmail.com>
2018-03-09 22:14:48 +0600

    wip

--- note/tests.py
+++ note/tests.py
@@ -1,1 +1,9 @@
-
+from django.urls import resolve, reverse
+from django.test import TestCase, TransactionTestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from . import views
+from base.models import Account, Setting
+from .models import NoteBook, Notes
+from freezegun import freeze_time
+from django.utils import timezone
