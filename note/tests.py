0271f637ba334fe7786f7f10f9f3722c178c8b1e
Ashutosh Das <git.pyprism@gmail.com>
2018-03-12 19:20:01 +0600

    added test

--- note/tests.py
+++ note/tests.py
@@ -7,3 +7,16 @@ from base.models import Account, Setting
 from .models import NoteBook, Notes
 from freezegun import freeze_time
 from django.utils import timezone
+
+
+class NotebooksViewTest(TestCase):
+
+    def setUp(self):
+        self.user = Account.objects.create_user(username='hiren', password="xyz")
+        self.c = Client()
+
+    def test_login_url_resolves_to_login_view(self):
+        found = resolve(reverse('notebook'))
+        self.assertEqual(found.func, views.notebooks)
+
+
