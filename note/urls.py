e519f6bf1c21b8395e712205779b49e67ba5d126
Ashutosh Das <git.pyprism@gmail.com>
2018-03-15 16:42:39 +0600

    added new route

--- note/urls.py
+++ note/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create/', views.notebook_create, name='notebook_create'),
     path('<int:pk>/create/', views.note_create, name='note_create'),
     path('note/<int:pk>/delete/', views.note_delete, name='note_delete'),
+    path('note/<int:pk>/edit/', views.note_edit, name='note_edit'),
     path('note/<int:pk>/', views.note_by_id, name='note_by_id'),
 ]
 
