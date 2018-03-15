0c957d20cbd00d2b6d3ce77501e10173d1d3a30d
Ashutosh Das <git.pyprism@gmail.com>
2018-03-15 16:06:05 +0600

    fixed div

--- static/components/note_by_id.js
+++ static/components/note_by_id.js
@@ -7,12 +7,60 @@ class NoteById extends React.Component{
     constructor(){
         super();
         this.state = {
-            loading: true
+            loading: true,
+            data: ""
         }
     }
 
-    bunny(){
+    loadData(){
+        $.ajax(window.location.pathname, {
+            contentType: "application/json",
+            success: function(data) {
+                this.setState({data: data});
+                this.setState({loading: false});
+            }.bind(this),
+            error: function(data) {
+                console.error(data);
+            }
+        });
+    }
 
+    componentDidMount(){
+        this.loadData();
+    }
+
+    bunny(){
+        return(
+            <div>
+                <div className="header">
+                    <h2>
+                        {this.state.data[0]["fields"]["title"]}
+                    </h2>
+                </div>
+                <div  className="body">
+                    {this.state.data[0]["fields"]["content"]}
+                    <br/>
+                    <br/>
+                    <div className="row clearfix">
+                        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <a href="#">
+                                <button type="button" className="btn btn-info waves-effect">
+                                    <i className="material-icons">edit</i>
+                                    <span>Edit</span>
+                                </button>
+                            </a>
+
+                            <a href="#">
+                                <button type="button" className="btn btn-danger waves-effect">
+                                    <i className="material-icons">delete</i>
+                                    <span>Delete</span>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     render() {
