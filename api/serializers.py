7940f5ac9b48bafcdfc38ae014a9e165cf516379
Ashutosh Das <git.pyprism@gmail.com>
2018-04-01 14:32:43 +0600

    removed field from serializer

--- api/serializers.py
+++ api/serializers.py
@@ -29,8 +29,8 @@ class NotesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Notes
-        fields = ('id', 'note_book', 'title', 'content', 'encrypted', 'iv',
-                  'salt', 'unique_id', 'created_at', 'updated_at', 'user')
+        fields = ('id', 'note_book', 'title', 'content', 'encrypted',
+                  'unique_id', 'created_at', 'updated_at', 'user')
 
     def create(self, validated_data):
         notebook_data = validated_data.pop('note_book')
@@ -49,8 +49,6 @@ class NotesSerializer(serializers.ModelSerializer):
         instance.title = validated_data.get('title', instance.title)
         instance.content = validated_data.get('content', instance.content)
         instance.encrypted = validated_data.get('encrypted', instance.encrypted)
-        instance.iv = validated_data.get('iv', instance.iv)
-        instance.salt = validated_data.get('salt', instance.salt)
         instance.synced_desktop = validated_data.get('synced_desktop', instance.synced_desktop)
         instance.synced_mobile = validated_data.get('synced_mobile', instance.synced_mobile)
         instance.unique_id = validated_data.get('unique_id', instance.unique_id)
