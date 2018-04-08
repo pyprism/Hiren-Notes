e54353eb58a9adfb3b5cc1f96c0413e17743b0c8
Ashutosh Das <git.pyprism@gmail.com>
2018-04-08 21:33:47 +0600

    Added new route

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('settings/', views.settings, name='settings'),
     path('secret/', views.secret_code, name='secret_code'),
     path('logout/', logout, {'next_page': '/'}, name='logout'),
 ]
